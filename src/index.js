d89d667f4e8da0dd5866be08722c4e002c86844a
Pavel <paveliva1988@gmail.com>
2025-01-10 10:08:47 +0300

    add anchor to section and bind anchor link to nav btn

--- src/index.js
+++ src/index.js
@@ -4,16 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from "react-router";
 import { store } from './store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <App />
     </Provider>
   </React.StrictMode>
 );
